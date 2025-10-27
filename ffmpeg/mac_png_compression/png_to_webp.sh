2031d268f5b3e7116f4f2e61642ec8d6022c4f19
Pedro Lanhoso <pedrolanhoso@Pedros-MacBook-Pro.local>
2025-10-27 09:48:12 +0000

    85%

--- ffmpeg/mac_png_compression/png_to_webp.sh
+++ ffmpeg/mac_png_compression/png_to_webp.sh
@@ -1,3 +1,9 @@
+# (85% Compression)
+for f in *.png; do
+  ffmpeg -i "$f" -vf "scale=w=min(iw\,1920):h=-1" -compression_level 6 -q:v 85 "${f%.*}.webp"
+done
+
+# (80% Compression)
 for f in *.png; do
   ffmpeg -i "$f" -vf "scale=w=min(iw\,1600):h=-1" -compression_level 6 -q:v 80 "${f%.*}.webp"
 done
